5a97370e6c9e61a987f9eb9a26df28c2e1b6e951
abdelrahman rouchdi <119289862+ar77771450@users.noreply.github.com>
2023-10-13 17:09:22 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Function to start the game
+function startGame() {
+    // Implement logic to start the game
+    // Initialize speech recognition and capture the user's speech
+    // Compare the speech with the card text and provide feedback
+    console.log("Game started!");
+}
